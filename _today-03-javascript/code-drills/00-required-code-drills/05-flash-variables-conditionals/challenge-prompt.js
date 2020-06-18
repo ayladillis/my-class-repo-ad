493e87ea999ae0517b9c98d2f6fc209817b84b35
Ayla Dillis <aayladillis@gmail.com>
2020-06-18 09:20:51 -0700

    code drills practice

--- _today-03-javascript/code-drills/00-required-code-drills/05-flash-variables-conditionals/challenge-prompt.js
+++ _today-03-javascript/code-drills/00-required-code-drills/05-flash-variables-conditionals/challenge-prompt.js
@@ -5,63 +5,84 @@ var farm;
 // set that farm variable to be an array with the elements
 // Pig, Cow, Chicken, Dog, Horse, Sheep
 
-
+farm = ["Pig", "Cow", "Chicken", "Dog", "Horse", "Sheep"];
 
 // write an if statement that creates an alert that says "RWAR!" if the first element of farm is not "godzilla"
 
-
+if (farm[0] !== "Godzilla") {
+    alert("RAWR");
+}
 
 // write another if statement that creates an alert "SCREECH!" if the last element of farm is "mothra"
 
-
+if (farm[farm.length - 1] === "Mothra") {
+    alert("SCREECH!");
+}
 
 // declare a variable named dog with a value of "Spot"
 
+var dog = "Spot"
 
 // declare 3 variables cat, car, city without assigning them values
 
+var cat, car, city; 
 
 // assign the string "Farley" to cat
 
+cat = "Farley";
 
 // assign the string "San Francisco" to city
 
+city = "San Francisco";
 
 // assign the string "Prius" to car
 
+car = "Prias";
 
 // using string concatenation, console log out the sentence 
 // "See Spot run!" without explicitly using "Spot" inside the string.
-
+console.log("See " + dog + "run!");
 
 // using string concatenation, console log out the sentence 
 //"I drive Farley around San Francisco in my Prius" without explicityly 
 //using "Farley", "San Francisco", and "Prius"
 // Remember to use the variables we just declared!
 
+console.log("I drive " + cat + "around" + city + "in my " + car);
 
 // declare a variable budget and assign it a value of 5000
 
+var budget = 5000; 
 
 // declare a variable rentCost and assign it a value of 1500
 
+var rentCost = 1500;
 
 // declare a variable utilitiesCost and assign it a value of 150
 
+var utilitiesCost = 150; 
 
 // declare a variable foodCost and assign it a value of 250
 
+var foodCost = 250;
 
 // declare a variable transportationCost and assign it a value of 350
 
+var transportationCost = 350;
 
 // declare a variable computerCost and assign it a value of 2000
 
+var computerCost = 2000;
 
 // write an if statement that checks whether the sum of all our costs is within the budget.
 // if it is within budget, console log out the total cost, otherwise, create an alert telling
 // us the sum of all the costs.
 
+if ((rentCost + utilitiesCost + foodCost + transportationCost + computerCost) <= budget) {
+    console.log("Budget is OK") 
+} else { 
+    alert(rentCost + utilitiesCost + foodCost + transportationCost + computerCost);
+}
 
 
 // write an if statement that checks whether the rentCost is larger than 
@@ -69,5 +90,12 @@ var farm;
 // and if computerCost is greater than the sum of rentCost and utilities cost
 // if both those statements are true, then create an alert that says "Computer costs are too high!"
 // else, create an alert that says "Rent is too high!"
+
+if ((rentCost > (utilitiesCost + foodCost + transportationCost)) &&
+(computerCost > (rentCost + utilitiesCost))){
+    alert("Computer costs are too hight!");
+} else {
+    alert("Rent is too high");
+}
 
 
--- _today-03-javascript/code-drills/00-required-code-drills/06-core-js-functions-1/challenge-prompt.js
+++ _today-03-javascript/code-drills/00-required-code-drills/06-core-js-functions-1/challenge-prompt.js
@@ -1,6 +1,8 @@
 // define a function "fun" that console logs "Functions are FUN!"
 
-
+function fun() {
+    console.log("Functions are FUN!");
+}
 
 // before moving on, open index.html and see what console logs have been made.
 // why doesn't "Functions are FUN! appear yet?"
@@ -8,65 +10,100 @@
 
 Your Answer here
 
+the function needs to be called on inorder to execute it content.
+
 */
 
 
 // define a function "thirtySeven" that console logs the sum of 18 and 19
 
-
+function thirtySeven() {
+    console.log(18 + 19);
+}
 
 // call two functions you've defined so far
 
+fun();
+thirtySeven();
 
 // define a function "log" that takes in an argument and console logs that argument
 
-
+function log(arg) {
+    console.log(arg);
+}
 
 // define a function "hello" that calls on the "log" function to console log "Hello World"
 
-
+function hello() {
+    console.log("Hello World");
+}
 
 // call your "hello" function
 
+hello();
 
 // define a function "popUp" that creates an alert that says "This is an alert."
 
-
+function popUp() {
+    alert("This is an alert");
+}
 
 // define a function "noPopUp" that creates an alert that says "Okay, you won't get a pop up."
 
+function noPopUp() {
+    alert("Okay, you won't get a pop up.");
+}
 
 
 // define a function "goodDay" that creates a confirm dialogue asking the user if they are having a nice day
 // and uses your "log" function to display the response
 
-
+function goodDay() {
+    var response = confir("Are you having a nice day?");
+    log(response);
+}
 
 // call your "goodDay" function
 
+goodDay();
 
 // define a function "userPopUp" that creates a confirm dialogue asking the user if they would like a popup
 // if they answer yes, call upon your "popUp" function; otherwise, call upon your "noPopUp" function
 
-
+function userPopUp() {
+    var response = confirm("Would you like a popup?");
+    if (response) {
+        popUp();
+    } else {
+        noPopUp();
+    }
+}
 
 // call your "userPopUp" function
 
+userPopUp();
 
 // define a function "userName" that prompts the user for their name and then 
 // uses your "log" function to display the response
 
-
+function userName() {
+    var response = prompt("Enter name here");
+    log(response);
+}
 
 // call your "userName" function
 
+userName(); 
 
 // define a function "favoriteFruit" that prompts the user for their favorite fruit
 // and the creates an alert that displays the response
 
-
+function favoriteFruit() {
+    var response = prompt("What is your favorite fruit?");
+    alert(response);
+}
 
 // call your "favoriteFruit" function
 
-
+favoriteFruit();
 
